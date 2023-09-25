d3af49492395634d5223ba3edf3e17dddc07c458
Jasmine T <tanghocle456@gmail.com>
2023-09-25 02:01:32 -0700

    Add carousel based on mouse movement

--- name_changing.js
+++ name_changing.js
@@ -1,31 +1,34 @@
-let jsonData;
+document.addEventListener("DOMContentLoaded", (event) => {
+    let jsonData;
 
-fetch('./names_with_styles.json')
-    .then(response => response.json())
-    .then(data => {
-        // Assign the data to the variable
-        jsonData = data;
+    fetch('./names_with_styles.json')
+        .then(response => response.json())
+        .then(data => {
+            // Assign the data to the variable
+            jsonData = data;
 
-        let i = 0;
-        let jsonKeys = Object.keys(jsonData);
-        let jsonValues = Object.values(jsonData);
-        const spanElement = document.getElementById('changing-names');
-        function start(counter) {
+            let i = 0;
+            let jsonKeys = Object.keys(jsonData);
+            let jsonValues = Object.values(jsonData);
+            const spanElement = document.getElementById('changing-names');
+            function start(counter) {
 
-            setTimeout(function () {
-                nameData = jsonValues[counter];
-                console.log(nameData['name']);
-                console.log(nameData['color']);
-                spanElement.textContent = nameData["name"];
-                spanElement.style.color = nameData["color"];
-                counter = (counter + 1) % jsonValues.length;
-                start(counter);
-            }, 1500);
+                setTimeout(function () {
+                    nameData = jsonValues[counter];
+                    spanElement.textContent = `''${nameData["name"]}"`;
+                    spanElement.style.color = nameData["color"];
+                    spanElement.style.fontStyle = "italic";
+                    counter = (counter + 1) % jsonValues.length;
+                    start(counter);
+                }, 1500);
 
-        }
-        start(0);
-    })
-    .catch(error => {
-        // Handle any errors
-        console.log(error);
-    });
+            }
+            start(0);
+        })
+        .catch(error => {
+            // Handle any errors
+            console.log(error);
+        });
+
+})
+
--- nav.js
+++ nav.js
@@ -1,10 +1,15 @@
-fetch('nav.html')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#replace_with_navbar");
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem, oldelem);
-    })
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    fetch('nav.html')
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#replace_with_navbar");
+            let newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem, oldelem);
+        })
+
+})
+
 
 
