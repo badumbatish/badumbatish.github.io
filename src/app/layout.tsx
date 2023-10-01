ec61d64cfca15cd82b70629bbac32b991f988c87
Jasmine T <tanghocle456@gmail.com>
2023-10-01 16:39:56 -0700

    Add icon and new name for website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Favicon from '/public/favico.ico';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'jjasmine website',
+  title: 'jjasmine',
   description: 'Built with NextJS, TailwindCSS, and a tonnn of loveee :)',
+  icons: [{ rel: 'icon', url: Favicon.src }],
+
 }
 
 export default function RootLayout({
@@ -15,7 +18,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="en">
+
+
       <body className={inter.className}>{children}</body>
     </html>
   )
