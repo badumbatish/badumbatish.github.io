1d2ac5ebdc6d36603168c9d8b6fdaecc8347f34b
Jasmine T <tanghocle456@gmail.com>
2024-04-28 00:04:59 -0700

    Switched to astro

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'jjasmine\'s portfolio';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const USER_NICKNAME = 'jjasmine';
+export const USER_DESCRIPTION = 'jjasmine';
+export const TRANSITION_API = true;
