2ca6cd61999807f06d4dae076bbbed25d3628a4b
Jasmine T <tanghocle456@gmail.com>
2024-05-02 00:57:06 -0700

    Fix display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,8 @@ function TimePiece({date, title, experience, placement}
         <>
             <div className={`mx-4 pb-2 flex content-start`}>
                 <div>
-                <div className="mb-0 flex flex-row grow content-center justify-between gap-2 flex-1">
-                    <div className="flex flex-row items-center">
-                        <h1 className={"mx-3  font-bold text-xl"}>{title}</h1>
-                    </div>
+                <div className="ml-2 flex flex-row grow content-center justify-between gap-2 flex-1">
+                    <h1 className={"mx-3  font-bold text-xl"}>{title}</h1>
                     <h1 className={"italic font-light  items-center"}>{date}</h1>
 
                 </div>
@@ -50,7 +48,7 @@ function TimePiece({date, title, experience, placement}
 
 function TimeLine({children} : { children : JSX.Element[]}) {
     function TimeLineOrchestrator({props} : { props: JSX.Element[] }) {
-        return (<div className={"flex flex-col item-start md:items-center"}>
+        return (<div className={"flex flex-col item-start items-center"}>
             {props.map((value, index) => {
                 return <div className={" sm:w-1/2 md:w-4/6 text-base"} key={index}>{value}</div>;
             })}
