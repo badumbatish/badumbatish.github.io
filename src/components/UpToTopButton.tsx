b72ba2d266d0ccc84a23eff1cb270fccecb3e9de
Jasmine T <tanghocle456@gmail.com>
2024-05-04 03:23:11 -0700

    Fix button

--- src/components/UpToTopButton.tsx
+++ src/components/UpToTopButton.tsx
@@ -4,11 +4,13 @@
 import Image from "next/image";
 import React from "react";
 
+const a =             <Image src={"Arrow_top.png"} alt={"arrow to top"} height={30} width={30}></Image>
 
 const UpToTopButton: React.FC = () => {
     return (
         <button className={"bottom-4 right-4 fixed"} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-            <Image src={"Arrow_top.png"} alt={"arrow to top"} height={30} width={30}></Image>
+            <div  className={"inline text-2xl font-bold border-black border-2 rounded  p-0 m-0 h-100 w-100"}>&#x21e1;&#x21e1;</div>
+
         </button>
     );
 };
