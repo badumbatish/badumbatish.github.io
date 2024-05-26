bd831db143afd5c8f800578aab064c9a4cfa1c5d
jjasmine <tanghocle456@gmail.com>
2024-05-26 14:02:40 -0700

    Direct user to resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,19 @@ export default function Home() {
             <ExperienceSection className={sectionClassName}>
                 <TimeLine>
                     <TimePiece date={"May -> Aug 2024"} title={"Google Summer Of Code Participant with GCC"} experience={[
-                        `Work work work! Jasmine's going to work on gccrs (the GNU Compiler for Rust) :), providing inline assembly support
-                        for it. The project focuses on adding implementation for two built-in Rust macros: asm!(), and global_asm!(). `,
-                    `gccrs will be able to detect and parse the assembly code within asm!, and global_asm! macro, converting them to gcc assembly format to eventually generate code.`]}/>
+                        `Visit my resume for more information about my work.`
+                    //     `Work work work! Jasmine's going to work on gccrs (the GNU Compiler for Rust) :), providing inline assembly support
+                    //     for it. The project focuses on adding implementation for two built-in Rust macros: asm!(), and global_asm!(). `,
+                    // `gccrs will be able to detect and parse the assembly code within asm!, and global_asm! macro, converting them to gcc assembly format to eventually generate code.`
+                    ]}/>
 
                     <TimePiece date={"Jun -> Aug 2023"} title={"Software Intern @ Fermilab"} experience={[
-                        "Implemented efficient data processing for roughly 19,000 HDF5 files with Python’s multiprocessing, and Big O " +
-                        "complexity, reducing processing time from 80 hours to just 10 hours.",
-                        "Wrote a custom TOML-like interpreter in Python for the physics simulation program to aid in brute-forcing the optimization search space.",
-                        "Configured and built multi-core physics simulation program G4beamline with CMake"]}/>
+                        `Visit my resume for more information about my work.`
+                        // "Implemented efficient data processing for roughly 19,000 HDF5 files with Python’s multiprocessing, and Big O " +
+                        // "complexity, reducing processing time from 80 hours to just 10 hours.",
+                        // "Wrote a custom TOML-like interpreter in Python for the physics simulation program to aid in brute-forcing the optimization search space.",
+                        // "Configured and built multi-core physics simulation program G4beamline with CMake"
+                        ]}/>
                     <></>
                 </TimeLine>
             </ExperienceSection>
