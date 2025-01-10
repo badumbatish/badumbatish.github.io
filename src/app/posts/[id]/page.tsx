ed7c8b9713999aa4437ad30cf2aa2097ef16d1c3
badumbatish <tanghocle456@gmail.com>
2025-01-10 14:24:00 -0800

    Fix blog heading

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getPostById, getAllPosts } from "@/lib/api";
-import 'katex/dist/katex.min.css' // for styling math
+import 'katex/dist/katex.min.css'
+import Link from "next/link"; // for styling math
 
 
 // Set the title of the page to be the post title, note that we no longer use
@@ -35,7 +36,11 @@ export default async function Post(
 
     const { html, title, date } = await getPostById(id);
     return (
-        <article className="p-8 prose  max-w-none w-full lg:w-1/2 md:w-4/6 sm:w-5/6 prose-sky mx-auto">
+        <div className="py-12 flex flex-col items-center  justify-items-start mx-auto ">
+        <Link href={"/blog"} className={"flex justify-center text-4xl"}>
+            <h2>My blog</h2>
+        </Link>
+        <article className="p-8 prose pt-0.5 max-w-none w-full lg:w-1/2 md:w-4/6 sm:w-5/6 prose-sky mx-auto">
             <div className="flex justify-center text-2xl font-bold ">
                 <h2>{title}</h2>
             </div>
@@ -44,13 +49,15 @@ export default async function Post(
             </div>
             <div className="flex font-bold">Edit:</div>
             My resume is <a
-            href={"https://www.overleaf.com/project/638120c30d0003f7bfe10360"}>here</a> (and downloadable <a href={"https://badumbatish.github.io/about_me/Jasmine_Resume_Badumbatish.pdf"}>here</a>).
+            href={"https://www.overleaf.com/project/638120c30d0003f7bfe10360"}>here</a> (and downloadable <a
+            href={"https://badumbatish.github.io/about_me/Jasmine_Resume_Badumbatish.pdf"}>here</a>).
             If you know of a compiler related job posting, please feel free to contact me or refer me at either <a
             href={"tanghocle456@gmail.com"}>tanghocle456@gmail.com</a> or <a
             href={"jjasmine@berkeley.edu"}>jjasmine@berkeley.edu</a>.
             <hr></hr>
             <div dangerouslySetInnerHTML={{__html: html}}/>
         </article>
+        </div>
     );
 }
 
