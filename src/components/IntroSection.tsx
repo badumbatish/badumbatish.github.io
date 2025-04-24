dcf32d316851ee56ae150cbdc7f6283b6827a666
badumbatish <tanghocle456@gmail.com>
2025-04-23 17:54:35 -0700

    oh nice

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -117,7 +117,12 @@ const LeftMainCard = () => {
 
                         <LinkButton link={"https://github.com/badumbatish/"}
                                     linkName={"GitHub"}></LinkButton>
-                        <LinkButton link={"https://badumbatish.github.io/about_me/Jasmine_Resume_Badumbatish.pdf"} linkName={"resume.pdf"}></LinkButton>
+                        <LinkButton link={"https://badumbatish.github.io/about_me/Jasmine_Resume_Badumbatish.pdf"}
+                                    linkName={"resume.pdf"}></LinkButton>
+
+                        <LinkButton link={"https://discord.gg/mvdVEqDQxn"} linkName={"Discord"}></LinkButton>
+                        <LinkButton link={"https://ko-fi.com/badumbatish"} linkName={"My Kofi"}></LinkButton>
+
                         <LinkButton link={"https://leetcode.com/badumbatish/"}
                                     linkName={"Leetcode"}></LinkButton>
                         <LinkButton link={"https://gcc-rust.zulipchat.com/"} linkName={"gccrs Zulip"}></LinkButton>
